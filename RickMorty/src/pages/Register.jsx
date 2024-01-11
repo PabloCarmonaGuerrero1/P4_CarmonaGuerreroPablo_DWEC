76b81ba60caec1c00111cff772c18b926a2434e3
PabloCarmonaGuerrero1 <pcrgue233@g.educaand.es>
2024-01-11 10:57:55 +0100

    clase

--- RickMorty/src/pages/Register.jsx
+++ RickMorty/src/pages/Register.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    saveUserData(formData);
+
+  };
+
+  const saveUserData = async (userData) => {
+    const db = new Dexie('UserDataDB');
+    db.version(1).stores({
+      users: '++id,username,email,password',
+    });
+
+    try {
+      await db.users.add(userData);
+      console.log('Usuario registrado correctamente');
+    } catch (error) {
+      console.error('Error al registrar el usuario', error);
+    }
+  };
+
   return (
     <div>
       <h1>Registro</h1>
-      <p>Esta es la página de register de mi aplicación.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Usuario:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Correo Electrónico:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Registrar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- RickMorty/src/pages/User.jsx
+++ RickMorty/src/pages/User.jsx
@@ -1,73 +1,101 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import './User.css';
-import Pagination from 'react-paginate';
 
 const User = () => {
   const [user, setUser] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [characters, setCharacters] = useState([]);
   const [selectedCharacter, setSelectedCharacter] = useState(null);
-  const [currentPage, setCurrentPage] = useState(0);
   const charactersPerPage = 20;
+  const charactersPerRow = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxId, setMaxId] = useState(1);
+  const [characterData, setCharacterData] = useState([]);
+
+  const fetchCharacters = (page) => {
+    fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+      .then(response => response.json())
+      .then(data => {
+        setCharacterData(data.results);
+        setMaxId(data.info.pages);
+      })
+      .catch(error => {
+        console.error("Error al llamar a la API:", error);
+      });
+  };
 
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/1')
       .then(response => response.json())
       .then(data => setUser(data));
-  }, []);
 
-  useEffect(() => {
-    if (!user) return;
     fetch('https://rickandmortyapi.com/api/character')
       .then(response => response.json())
       .then(data => setCharacters(data.results));
-  }, [user]);
+  }, []);
 
   const openModal = () => setModalIsOpen(true);
 
-  const closeModal = () => setModalIsOpen(false);
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setCurrentPage(1);
+  };
+
+  const handleCharacterClick = (character) => {
+    setSelectedCharacter(character);
+    setModalIsOpen(true);
+  };
+
+  useEffect(() => {
+    fetchCharacters(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   const indexOfLastCharacter = (currentPage + 1) * charactersPerPage;
   const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
   const currentCharacters = characters.slice(indexOfFirstCharacter, indexOfLastCharacter);
 
-  const handleCharacterClick = (character) => setSelectedCharacter(character);
+  const charactersGrid = [];
+  for (let i = 0; i < currentCharacters.length; i += charactersPerRow) {
+    charactersGrid.push(currentCharacters.slice(i, i + charactersPerRow));
+  }
 
   return (
     <div>
       <h1>Perfil de usuario</h1>
-      {user ? (
+      {user && (
         <>
-          <img src={selectedCharacter ? selectedCharacter.image : user.image} alt={user.name} onClick={openModal} />
+          <img
+            src={selectedCharacter ? selectedCharacter.image : user.image}
+            alt={selectedCharacter ? selectedCharacter.name : user.name}
+            onClick={openModal}
+          />
           <p>{user.name}</p>
         </>
-      ) : (
-        <p>Cargando perfil de usuario...</p>
       )}
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="Modal" overlayClassName="Overlay">
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>  
         <h2>Selecciona un personaje</h2>
-        <ul className="characters-grid">
-          {currentCharacters.map((character, index) => (
-            <li key={index} onClick={() => handleCharacterClick(character)}>
-              <img src={character.image} alt={character.name} />
-              <p>{character.name}</p>
-            </li>
-          ))}
-        </ul>
-        <Pagination
-          previousLabel={'Anterior'}
-          nextLabel={'Siguiente'}
-          breakLabel={'...'}
-          pageCount={Math.ceil(characters.length / charactersPerPage)}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={(data) => setCurrentPage(data.selected)}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'}
-        />
-        <button onClick={closeModal}>Cerrar</button>
+        <div>
+        {characterData.map(character => (
+          <div key={character.id} style={{ marginBottom: 20, marginRight: 20, float: 'left' }}>
+            <img src={character.image} alt="Imagen del personaje" onClick={() => handleCharacterClick(character)}/>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+          Anterior
+        </button>
+        <span>Página {currentPage} de {maxId}</span>
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === maxId}>
+          Siguiente
+        </button>
+      </div>
+      <button onClick={closeModal}>Cerrar</button>
       </Modal>
     </div>
   );
