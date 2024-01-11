d1ce88f13611ad2d3bbf8e17af80d6670752792e
PabloCarmonaGuerrero1 <pcrgue233@g.educaand.es>
2024-01-11 07:20:49 +0100

    enlazando a clase

--- RickMorty/src/pages/Login.jsx
+++ RickMorty/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h1>Registro</h1>
+      <h1>Login</h1>
       <p>Esta es la página de login de mi aplicación.</p>
     </div>
   )
