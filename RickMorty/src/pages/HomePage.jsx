61683fef977ec82cf7c520f00724622da89f7fa8
PabloCarmonaGuerrero1 <pcrgue233@g.educaand.es>
2024-01-15 10:25:22 +0100

    clase

--- RickMorty/src/pages/HomePage.jsx
+++ RickMorty/src/pages/HomePage.jsx
@@ -1,87 +1,109 @@
 import React, { useState, useEffect } from "react";
 import "./HomePage.css";
 import Modal from 'react-modal';
+import { useUser } from "./UserContext";
+import { openDB } from 'idb';
+import { DBConfig } from "../DataBase/DBConfig";
 
 const HomePage = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [maxId, setMaxId] = useState(1);
-    const [characterData, setCharacterData] = useState([]);
-    const [selectedStatus, setSelectedStatus] = useState("all");
-    const [searchName, setSearchName] = useState("");
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [selectedCharacter, setSelectedCharacter] = useState(null);
-  
-  
-    const fetchCharacters = (page, status, name) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxId, setMaxId] = useState(1);
+  const [characterData, setCharacterData] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState("all");
+  const [searchName, setSearchName] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const { user } = useUser();
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const fetchCharacters = (page, status, name) => {
       let url = `https://rickandmortyapi.com/api/character?page=${page}`;
-  
-  
+
       if (status !== "all") {
-        url += `&status=${status}`;
+          url += `&status=${status}`;
       }
-  
-  
+
       if (name.trim() !== "") {
-        url += `&name=${name}`;
+          url += `&name=${name}`;
       }
-  
-  
+
       fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`Error en la llamada a la API: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          if (data.results && data.results.length > 0) {
-            setCharacterData(data.results);
-            setMaxId(data.info.pages);
-          } else {
-            setCharacterData([]);
-            setMaxId(1);
-          }
-        })
-        .catch((error) => {
-          console.error("Error al llamar a la API:", error);
-          setCharacterData([]);
-          setMaxId(1);
-        });
-    };
-  
-  
-    useEffect(() => {
+          .then((response) => {
+              if (!response.ok) {
+                  throw new Error(`Error en la llamada a la API: ${response.status}`);
+              }
+              return response.json();
+          })
+          .then((data) => {
+              if (data.results && data.results.length > 0) {
+                  setCharacterData(data.results);
+                  setMaxId(data.info.pages);
+              } else {
+                  setCharacterData([]);
+                  setMaxId(1);
+              }
+          })
+          .catch((error) => {
+              console.error("Error al llamar a la API:", error);
+              setCharacterData([]);
+              setMaxId(1);
+          });
+  };
+
+  useEffect(() => {
       fetchCharacters(currentPage, selectedStatus, searchName);
-    }, [currentPage, selectedStatus, searchName]);
-  
-  
-    const handlePageChange = (newPage) => {
+  }, [currentPage, selectedStatus, searchName]);
+
+  const handlePageChange = (newPage) => {
       setCurrentPage(newPage);
-    };
-  
-  
-    const handleStatusChange = (e) => {
+  };
+
+  const handleStatusChange = (e) => {
       setSelectedStatus(e.target.value);
-    };
-  
-  
-    const handleSearchChange = (e) => {
+  };
+
+  const handleSearchChange = (e) => {
       setSearchName(e.target.value);
-    };
-  
-  
-    const openModal = (character) => {
+  };
+
+  const openModal = (character) => {
       setSelectedCharacter(character);
       setModalIsOpen(true);
-    };
-  
-  
-    const closeModal = () => {
+  };
+
+  const closeModal = () => {
       setSelectedCharacter(null);
       setModalIsOpen(false);
-    };
-  
-  
+  };
+
+  const handleFavoriteClick = async () => {
+    if (selectedCharacter) {
+        const isFavorite = userFavorites.includes(selectedCharacter.id);
+
+        if (isFavorite) {
+            setUserFavorites((prevFavorites) => prevFavorites.filter(id => id !== selectedCharacter.id));
+        } else {
+            setUserFavorites((prevFavorites) => [...prevFavorites, selectedCharacter.id]);
+        }
+
+        try {
+            const db = await openDB(DBConfig.name, DBConfig.version);
+            const transaction = db.transaction(DBConfig.objectStoresMeta[0].store, 'readwrite');
+            const store = transaction.objectStore(DBConfig.objectStoresMeta[0].store);
+
+            const userRecord = await store.get(user.email);
+
+            if (userRecord && JSON.stringify(userRecord.favourite) !== JSON.stringify(userFavorites)) {
+                userRecord.favourite = userFavorites;
+                await store.put(userRecord);
+            }
+        } catch (error) {
+            console.error('Error al actualizar la lista de favoritos en la base de datos', error);
+        }
+    }
+};
+
+
     return (
       <div className="home-page-container">
         <div className="header">
@@ -120,20 +142,20 @@ const HomePage = () => {
           )}
         </div>
         <Modal className="modal" isOpen={modalIsOpen} onRequestClose={closeModal}>
-          {selectedCharacter && (
-            <>
-              <h2>{selectedCharacter.name}</h2>
-              <img src={selectedCharacter.image} alt="Imagen del personaje" />
-              <p>Status: {selectedCharacter.status}</p>
-              <p>Species: {selectedCharacter.species}</p>
-              <p>Gender: {selectedCharacter.gender}</p>
-              <p>Origin: {selectedCharacter.origin.name}</p>
-              <p>Location: {selectedCharacter.location.name}</p>
-            </>
-          )}
-          <button>Fav</button>
-          <button onClick={closeModal}>Close</button>
-        </Modal>
+                {selectedCharacter && (
+                    <>
+                        <h2>{selectedCharacter.name}</h2>
+                        <img src={selectedCharacter.image} alt="Imagen del personaje" />
+                        <p>Status: {selectedCharacter.status}</p>
+                        <p>Species: {selectedCharacter.species}</p>
+                        <p>Gender: {selectedCharacter.gender}</p>
+                        <p>Origin: {selectedCharacter.origin.name}</p>
+                        <p>Location: {selectedCharacter.location.name}</p>
+                    </>
+                )}
+                <button onClick={handleFavoriteClick}>Fav</button>
+                <button onClick={closeModal}>Close</button>
+            </Modal>
         <div className="footer">
           <div className="pagination-container">
             <button
@@ -154,6 +176,6 @@ const HomePage = () => {
       </div>
     );
   };
-  
+
 
 export default HomePage
--- RickMorty/src/pages/Login.jsx
+++ RickMorty/src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { openDB } from 'idb'; 
 import { DBConfig } from '../DataBase/DBConfig';
 import "./Login.css"
-
+import { useUser } from "./UserContext.jsx"
+import { useNavigate } from 'react-router-dom';
 
 const openDatabase = () => {
   return openDB(DBConfig.name, DBConfig.version, {
@@ -17,10 +18,13 @@ const openDatabase = () => {
 };
 
 const LoginForm = () => {
+  const { loginUser } = useUser(); 
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+
+  const navigate = useNavigate(); 
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,62 +36,65 @@ const LoginForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     try {
       const db = await openDatabase();
       const transaction = db.transaction(DBConfig.objectStoresMeta[0].store, 'readonly');
       const store = transaction.objectStore(DBConfig.objectStoresMeta[0].store);
-
+  
       const request = store.index('email').get(formData.email);
-
       const result = await request;
-
+  
       if (!result || result.password !== formData.password) {
         console.error('Credenciales inválidas');
         return;
       }
-
+  
       console.log('Usuario autenticado:', result);
-     
-      setFormData({
-        email: '',
-        password: '',
-      });
-      window.location.href = '/HomePage';
-
+  
+      loginUser(result);
+  
+      setTimeout(() => {
+        setFormData({
+          email: '',
+          password: '',
+        });
+        navigate('/homepage');
+      }, 500); 
     } catch (error) {
       console.error('Error al autenticar el usuario', error);
     }
   };
+  
   return (
     <div className='login-container'>
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <label>
-        Email
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-      <button onClick={() => window.location.href = '/Register'}>Register</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+        <button onClick={() => navigate('/Register')}>Register</button>
+      </form>
     </div>
   );
 };
